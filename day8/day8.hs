6b3e55eb16c01aa9e6f3f7d05d8afb09983ab84c
Richard Weber <riche.weber@gmail.com>
2023-12-08 15:03:52 +0200

    Part 2 in haskell

--- day8/day8.hs
+++ day8/day8.hs
@@ -5,10 +5,12 @@ split :: String -> (String, (String, String))
 split [a1, a2, a3, _, _, _, _, b1, b2, b3, _, _, c1, c2, c3, _] =
   ([a1, a2, a3] :: String, ([b1, b2, b3] :: String, [c1, c2, c3] :: String))
 
-navigate :: Int -> Map.Map String (String, String) -> (String, String) -> String -> Int
-navigate acc navigationSource current (x : xs)
-  | next == "ZZZ" = acc + 1
-  | otherwise = navigate (acc + 1) navigationSource (Map.findWithDefault ("foo", "foo") next navigationSource) xs
+type Checker = String -> Bool
+
+navigate :: Int -> Checker -> Map.Map String (String, String) -> (String, String) -> String -> Int
+navigate acc checker navigationSource current (x : xs)
+  | checker next = acc + 1
+  | otherwise = navigate (acc + 1) checker navigationSource (Map.findWithDefault ("foo", "foo") next navigationSource) xs
   where
     lr = if x == 'L' then fst else snd
     next = lr current
@@ -19,23 +21,12 @@ part1 lines = do
   let mapLines = drop 2 lines
   let navigationSource = Map.fromList (map split mapLines)
   let (Just start) = Map.lookup "AAA" navigationSource
-  navigate 0 navigationSource start instructions
+  let checker x = x == "ZZZ"
+  navigate 0 checker navigationSource start instructions
 
 endsIn :: Char -> String -> Bool
 endsIn x [_, _, y] = x == y
 
-navigate2 :: Int -> Map.Map String (String, String) -> [(String, String)] -> String -> Int
-navigate2 acc navigationSource currents (x : xs)
-  | allAtEnd = acc
-  | otherwise = navigate2 (acc + 1) navigationSource nexts xs
-  where
-    lr = if x == 'L' then fst else snd
-    nextKeys = map lr currents
-    allAtEnd = all (endsIn 'Z') nextKeys
-    nexts = map ( \x -> Map.findWithDefault ("foo", "foo") x navigationSource) nextKeys
-
--- this is just too slow, will not get there in a sane amount of time
--- solved with part2.ts instead for now... TODO: make a haskell version
 part2 :: [String] -> Int
 part2 lines = do
   let instructions = cycle (head lines)
@@ -44,10 +35,14 @@ part2 lines = do
   let keys = Map.keys navigationSource
   let startKeys = filter (endsIn 'A') keys
   let starts = map (\x -> Map.findWithDefault ("Foo", "Foo") x navigationSource) startKeys
-  navigate2 1 navigationSource starts instructions
+  let checker = endsIn 'Z'
+  let lengths = map (\x -> navigate 0 checker navigationSource x instructions) starts
+  foldl1 lcm lengths
+
 
 main :: IO ()
 main = do
   input <- readFile "input"
   let ls = lines input
   print (part1 ls)
+  print (part2 ls)
